0085bee4bbf3646557b209354558f840fe288a12
yassmzx <yassermoghder@gmail.com>
2019-09-25 19:47:28 +0200

    safsafas

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,5 @@
 public class Welcome {
    public static void main(String[] args) {
-     System.out.println("Welcome to Java20019!");
+     System.out.println( " fdfd Welcome to Java20019!");
    }
 }
